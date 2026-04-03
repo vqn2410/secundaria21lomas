506c736713b1a76885ca17deda50c18aec4a268c
vqn2410 <165409738+vqn2410@users.noreply.github.com>
2026-04-03 10:55:29 -0300

    Actualizar plataforma con nuevos módulos y optimizaciones

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDjk1O2zuxqMsuwWqTK8QqlU-AZsvEeqxU",
@@ -16,3 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
